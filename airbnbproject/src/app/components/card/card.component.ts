1574d10748e8ed79960ba4559096c1f40aeb439e
corentin&greg <corentin.gueniot27@gmail.com>
2023-10-13 09:53:39 +0200

    Commentaire de la Card component

--- airbnbproject/src/app/components/card/card.component.ts
+++ airbnbproject/src/app/components/card/card.component.ts
@@ -1,5 +1,8 @@
+// Import des modules Angular nécessaires
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+
+// Import des modèles et services
 import { City } from 'src/app/models/City';
 import { Logement } from 'src/app/models/Logement';
 import { GetAccommodationsService } from 'src/app/services/get-accommodations.service';
@@ -10,23 +13,24 @@ import { GetAccommodationsService } from 'src/app/services/get-accommodations.se
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-  @Input() cardData : Logement;
+  @Input() cardData: Logement;  // Input pour recevoir les données du composant parent
 
-  imageWithPrefix: string;
+  imageWithPrefix: string;  // Variable pour stocker l'URL de l'image avec le préfixe
 
   constructor(
-    private router: Router, 
+    private router: Router,
     private service: GetAccommodationsService
-  ){}
+  ) {}
 
   ngOnInit() {
-    // Concaténer le préfixe "data:image/jpeg;base64," avec le string d'encodage de l'image
+    // Concaténation du préfixe "data:image/jpeg;base64," avec le string d'encodage de l'image
     this.imageWithPrefix = "data:image/jpeg;base64," + this.cardData.image;
   }
 
-  public saveLiked(){
+  // Méthode appelée lorsqu'un utilisateur clique sur l'icône "like"
+  public saveLiked() {
     console.log("love !");
-    this.cardData.favourite = !this.cardData.favourite;
+    this.cardData.favourite = !this.cardData.favourite;  // Inversion de l'état de favori
     this.service.setAccomodations(this.cardData).subscribe(
       response => {
         console.log('Requête POST réussie :', response);
@@ -39,13 +43,10 @@ export class CardComponent {
     );
   }
 
-  public goToPage(cardData : Logement){
+  // Méthode appelée lorsqu'un utilisateur clique sur la carte pour voir plus de détails
+  public goToPage(cardData: Logement) {
     console.log("goToPage");
 
-    // this.serviceSaveData.setLogement(cardData);
-    
-    this.router.navigateByUrl('presentation-logement/'+cardData.id);
+    this.router.navigateByUrl('presentation-logement/' + cardData.id);  // Navigation vers une autre page
   }
-
-  
 }
